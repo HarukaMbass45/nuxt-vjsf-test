2e80e58c30033e5bf0fe40b3a89791a974145582
KentaNakanishi <kenta_nakanishi@rebuild-inc.co.jp>
2022-10-16 15:49:13 +0900

    initial commit

---
+++ pages/vuetify/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-text-field></v-text-field>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
